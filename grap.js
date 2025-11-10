abda5c398d8de2d2c196436eeafed7c0efa4e76d
Nadooor <nadersayed742@gmail.com>
2025-11-10 17:32:51 +0200

    Made the floor and the walls

---
+++ grap.js
@@ -0,0 +1,14 @@
+AFRAME.AFRAME.registerComponent('grap', {
+    schema: {
+        
+    },
+
+    init: function () {
+        
+
+    },
+
+    tick: function (time, timeDelta) {
+      // Do something on every scene tick or frame.
+    }
+});
